df457f2bd8ccac1e06afaa8f682467f1b325d228
quanghv20 <quanghv20@gmail.com>
2025-04-02 10:51:48 +0700

    Update

--- Ecommerce/app/src/main.jsx
+++ Ecommerce/app/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-import "./scss/main.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "scss/main.scss";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
--- Ecommerce/app/src/pages/Home/index.jsx
+++ Ecommerce/app/src/pages/Home/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+
+import ctgItem1 from "../../assets/imgs/category-item/item-1.png";
+import ctgItem2 from "../../assets/imgs/category-item/item-2.png";
+import ctgItem3 from "../../assets/imgs/category-item/item-3.png";
+
 import styles from "./styles.module.scss";
 
 export default function HomePage() {
   return (
-    <div className={styles.home_page}>
-      <div className={styles.slideshow}>
+    <div className={`${styles.home_page} container`}>
+      {/* Slideshow */}
+      <section className={styles.slideshow}>
         <svg
           width="1340"
           height="455"
@@ -14,7 +20,29 @@ export default function HomePage() {
         >
           <rect width="1340" height="455" rx="20" fill="#77DAE6" />
         </svg>
-      </div>
+      </section>
+
+      {/* Browse Categories */}
+      <section className={styles.browse_ctg}>
+        <h2 className={styles.heading}>Browse Categories</h2>
+        <div className="row g-5">
+          {[ctgItem1, ctgItem2, ctgItem3].map((item) => {
+            return (
+              <div className="col col-12 col-lg-4">
+                <article className={styles.ctg_item}>
+                  <img src={item} alt="" className={styles.ctg_item__thumb} />
+                  <section className={styles.ctg_item__info}>
+                    <h3 className={styles.ctg_item__title}>$24 - $150</h3>
+                    <p className={styles.ctg_item__desc}>
+                      New sumatra mandeling coffe blend
+                    </p>
+                  </section>
+                </article>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </div>
   );
 }
